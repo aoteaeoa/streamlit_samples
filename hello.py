d049851f5d141c361836851fab7a92e752bff991
aoteaeoa <128959102+aoteaeoa@users.noreply.github.com>
2024-09-01 17:29:12 +0900

    hello.py 修正

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title("hello world!")
+st.write("You'll never find a rainbow if you're looking down. -Charles Chaplin-")
+st.write("The flower that blooms in adversity is the rarest and most beautiful of all. -Walt Disney-")
