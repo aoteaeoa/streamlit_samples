8246bc398f4709a956ece4255df8463198ef7fff
aoteaeoa <128959102+aoteaeoa@users.noreply.github.com>
2024-09-01 17:48:24 +0900

    hello.py追加

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 import streamlit as st
-st.title("hello world!")
+st.title("Hello world!")
 st.write("You'll never find a rainbow if you're looking down. -Charles Chaplin-")
 st.write("The flower that blooms in adversity is the rarest and most beautiful of all. -Walt Disney-")
+st.write("The special secret of making dreams come true can be summarized in four C's. They are Curiosity, Confidence, Courage and Constancy. -Walt Disney-")
+st.write("I will prepare and some day my chance will come. -Abraham Lincoln-")
+st.write("Whatever you can do, or dream you can, begin it. Boldness has genius, power, and magic in it. -Johann Wolfgang von Goethe-")
